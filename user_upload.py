6d367ef6f866fadaabec6f1516c4ad22e870b7c1
adamelmohamad <37544116+adamelmohamad@users.noreply.github.com>
2021-09-18 14:33:04 +1000

    basic command line argument processing has been completed

--- user_upload.py
+++ user_upload.py
@@ -14,8 +14,6 @@ def full_run():
     host = re.search("-h .*", all_arguments).group().split(" ")[1]
     
 
-
-
 if "--help" in sys.argv and len(sys.argv) == 2:
     print("--file [csv file name] – this is the name of the CSV to be parsed \n--create_table – this will cause the PostgreSQL users table to be built (and no further action will be taken)\n--dry_run – this will be used with the --file directive in case we want to run the script but not insert into the DB. All other functions will be executed, but the database won't be altered\n-u – PostgreSQL username\n-p – PostgreSQL password\n-h – PostgreSQL host")
 elif "--file" in sys.argv and "--dry_run" in sys.argv and len(sys.argv) == 4:
